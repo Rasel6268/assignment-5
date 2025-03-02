e8554c96395f081532f5b2100896415cb9d81043
Rasel6268 <bmrasel626825@gmail.com>
2025-03-02 20:22:53 +0600

    blog_section

--- scripts/date.js
+++ scripts/date.js
@@ -1,7 +1,7 @@
 const dateInfo = document.getElementById('date')
 const currentDate = new Date;
 const formattedDate = currentDate.toDateString().split(' ')
-dateInfo.innerHTML = `<h1 class='text-xl font-medium'>${formattedDate[0]},<br>${formattedDate[1]} ${formattedDate[2]}</h1>`
+dateInfo.innerHTML = `<h1 class='text-xl font-medium'>${formattedDate[0]},${formattedDate[1]} ${formattedDate[2]}</h1>`
 
 const time  = currentDate.toLocaleTimeString();
 
